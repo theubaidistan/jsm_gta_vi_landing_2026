7778084987b5ae64a631c2be37d8bc95fb817c0d
theubaidistan <ubaidistan395@gmail.com>
2026-01-13 20:44:41 +0500

    Implement Second Video

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./sections/Hero";
 import Navbar from "./sections/Navbar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
+import SecondVideo from "./sections/SecondVideo";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -12,8 +13,12 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
+
       <FirstVideo />
       <Jason />
+
+      <SecondVideo />
+      {/* <Lucia/> */}
     </main>
   );
 };
