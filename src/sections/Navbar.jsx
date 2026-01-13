970686d337077f2fcf06667774d34f361efe94ce
theubaidistan <ubaidistan395@gmail.com>
2026-01-13 15:33:34 +0500

    Implement Hero Section

---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="./../public/images/nav-logo.svg" className="scale-90" />
+      <img src="./../public/images/menu.svg" className="w-10" />
+    </nav>
+  );
+};
+
+export default Navbar;
